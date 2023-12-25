dc2933eb4710e3fc046f8c77877f4c3b95354ab9
bishalcpgn <sbishal1589@gmail.com>
2023-12-25 12:45:03 +0545

    basic auth services added

---
+++ blogApp-Appwrite/src/auth/auth.js
@@ -0,0 +1,77 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+/*
+This file can be shared among other projects too.
+Workflow : 
+    make clas AuthService and export its object 
+    make obj of Client
+    add properties in constructor 
+    add other auth services
+*/
+
+class AuthService {
+    client = new Client()
+    account
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+
+        this.account(this.client)
+    }
+
+
+
+    async createAccount({ email, password, name }) {
+
+        try {
+            const useraccount = await this.account.create(ID.unique(), email, password, name)
+            if (useraccount) {
+                // login user after creating account  
+                return this.login({ email, password })
+            } else {
+                return useraccount
+            }
+
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async login({ email, password }) {
+
+        try {
+            await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error 
+        }
+    }
+
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+        }
+
+        return null
+    }
+
+
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error)
+        }
+    }
+
+
+}
+
+const authService = new AuthService()
+export default authService
