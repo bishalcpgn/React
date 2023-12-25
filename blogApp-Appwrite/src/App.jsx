1cf676447eef083d4cac09d4bf093d1e33e30390
bishalcpgn <sbishal1589@gmail.com>
2023-12-25 10:56:10 +0545

    project setup with env variables

---
+++ blogApp-Appwrite/src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>A blog app with appwrite</h1>
+
+
+    </div>
+  )
+}
+
+
+export default App
---
+++ blogApp-Appwrite/src/config/config.js
@@ -0,0 +1,16 @@
+const config = {
+
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+
+    appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+
+    appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+
+    appwrieBucketId: String(import.meta.env.VITE_BUCKET = ID)
+
+}
+
+
+
+
+export default config
